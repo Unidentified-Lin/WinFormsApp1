524047a914faf2dce212ad2e8aed7e364e8ac609
eric <eric@ivan.leathercraft.com.tw>
2022-03-14 11:00:30 +0800

    加入專案檔案。

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(21, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(265, 105)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "測試"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(21, 231)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(1105, 592)
+        Me.TextBox1.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(1165, 304)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 82
+        Me.DataGridView1.RowTemplate.Height = 40
+        Me.DataGridView1.Size = New System.Drawing.Size(784, 489)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(14.0!, 30.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1961, 865)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports Newtonsoft
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports RestSharp
+Imports System.IO
+Imports System.Linq
+Imports System.Net
+Imports System.Text
+
+Public Class Form1
+    Public Class Customer
+        Public Property Id As String
+        Public Property Email As String
+        Public Property First_name As String
+        Public Property Last_name As String
+        Public Property Phone As String
+        Public Property Note As String
+        Public Property Tags As String
+        Public Property State As String
+        Public Property Currency As String
+        Public Property Tax_exempt As String
+        Public Property Verified_email As String
+        Public Property Accepts_marketing As String
+        Public Property Accepts_marketing_updated_at As String
+        Public Property Orders_count As String
+        Public Property Total_spent As String
+        Public Property Last_order_id As String
+        Public Property Last_order_name As String
+        Public Property Created_at As String
+        Public Property Updated_at As String
+        Public Property Addresses As String
+
+    End Class
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim GetCustomers As String = "/admin/api/2020-10/customers.json"
+        'Dim ShopName As String = "Retail"
+        Dim Username As String = "ad2ce1bbb53cda0c801fc989e3ff304f"
+        Dim Password As String = "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"
+        Dim Shop As String = "ivanleathercraft"
+
+
+        Dim HasNext As Boolean = True
+        Dim Head As String = "https://" & Username & ":" & Password & "@" & Shop & ".myshopify.com"
+        Dim Resource As String = GetCustomers
+        Dim Query As String = "?limit=250"
+        Dim Url As String = Head + Resource + Query
+        Dim mLogiKey As Byte() = Encoding.Default.GetBytes(Username + ":" + Password)
+        Dim LoginKey As String = "Basic " + Convert.ToBase64String(mLogiKey)
+
+        Dim myWebRequest As WebRequest = WebRequest.Create(Url)
+        myWebRequest.ContentType = "application/JSON"
+        myWebRequest.Method = "GET"
+        myWebRequest.Headers.Add("Authorization", LoginKey)
+        Dim myWebResponse As WebResponse = myWebRequest.GetResponse()
+        Dim StreamRead As StreamReader = New StreamReader(myWebResponse.GetResponseStream())
+        Dim ProductsJSON As String = StreamRead.ReadToEnd()
+        Me.TextBox1.Text = ProductsJSON
+
+
+
+        MsgBox("OK !")
+    End Sub
+End Class
