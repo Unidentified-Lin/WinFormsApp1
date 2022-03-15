e660ab67749350b537f5f5f080b76a2a37e66bab
LinChePang <linchepang@ivan.leathercraft.com.tw>
2022-03-15 13:55:58 +0800

    Create paging btn.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,13 +25,15 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.NextPage = New System.Windows.Forms.Button()
+        Me.PreviousPage = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.Location = New System.Drawing.Point(10, 6)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(132, 52)
         Me.Button1.TabIndex = 0
@@ -43,7 +45,7 @@ Partial Class Form1
         Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.TextBox1.Location = New System.Drawing.Point(10, 78)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
@@ -59,22 +61,48 @@ Partial Class Form1
         Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Location = New System.Drawing.Point(421, 78)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 82
         Me.DataGridView1.RowTemplate.Height = 40
         Me.DataGridView1.Size = New System.Drawing.Size(805, 507)
         Me.DataGridView1.TabIndex = 2
         '
+        'NextPage
+        '
+        Me.NextPage.Font = New System.Drawing.Font("Microsoft JhengHei UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.NextPage.Location = New System.Drawing.Point(1149, 39)
+        Me.NextPage.Margin = New System.Windows.Forms.Padding(2)
+        Me.NextPage.Name = "NextPage"
+        Me.NextPage.Size = New System.Drawing.Size(77, 35)
+        Me.NextPage.TabIndex = 3
+        Me.NextPage.Text = "→"
+        Me.NextPage.UseVisualStyleBackColor = True
+        Me.NextPage.Visible = False
+        '
+        'PreviousPage
+        '
+        Me.PreviousPage.Font = New System.Drawing.Font("Microsoft JhengHei UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.PreviousPage.Location = New System.Drawing.Point(421, 39)
+        Me.PreviousPage.Margin = New System.Windows.Forms.Padding(2)
+        Me.PreviousPage.Name = "PreviousPage"
+        Me.PreviousPage.Size = New System.Drawing.Size(77, 35)
+        Me.PreviousPage.TabIndex = 4
+        Me.PreviousPage.Text = "←"
+        Me.PreviousPage.UseVisualStyleBackColor = True
+        Me.PreviousPage.Visible = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1241, 591)
+        Me.Controls.Add(Me.PreviousPage)
+        Me.Controls.Add(Me.NextPage)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -86,4 +114,6 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents NextPage As Button
+    Friend WithEvents PreviousPage As Button
 End Class
