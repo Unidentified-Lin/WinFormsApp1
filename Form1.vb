16bf474cd2ee8612312f0a1baa42b4a4243fe4ad
LinChePang <linchepang@ivan.leathercraft.com.tw>
2022-03-17 16:43:08 +0800

    Move sensitive information into yml file.

--- Form1.vb
+++ Form1.vb
@@ -1,55 +1,28 @@
-﻿Imports Newtonsoft.Json
-Imports ShopifySharp
+﻿Imports ShopifySharp
 Imports ShopifySharp.Lists
 Imports System.IO
-Imports System.Net
 Imports System.Net.Http
 Imports System.Net.Http.Json
 Imports System.Text
-Imports System.Text.Json
-Imports System.Text.Json.Serialization
 
 Public Class Form1
+    Private ReadOnly _retailEnvFile As String = "retail_env.yml"
+    Private _retailEnv As IDictionary(Of String, String)
 
     Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Dim GetCustomers As String = "/admin/api/2020-10/customers.json"
-        'Dim ShopName As String = "Retail"
-        Dim Username As String = "ad2ce1bbb53cda0c801fc989e3ff304f"
-        Dim Password As String = "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"
-        Dim Shop As String = "ivanleathercraft"
-
-
-        Dim HasNext As Boolean = True
-        Dim Head As String = "https://" & Username & ":" & Password & "@" & Shop & ".myshopify.com"
-        Dim Resource As String = GetCustomers
-        Dim Query As String = "?limit=20"
-        Dim Url As String = Head + Resource + Query
-        Dim mLogiKey As Byte() = Encoding.Default.GetBytes(Username + ":" + Password)
-        Dim LoginKey As String = "Basic " + Convert.ToBase64String(mLogiKey)
+        Dim exsist = File.Exists(_retailEnvFile)
+        _retailEnv = DotEnvFile.LoadFile(_retailEnvFile, True)
 
         'Using HttpClient (replace Async Function with Sub)
         'Dim result = Await GetRequestWithHttpClient(Of CustomerList)(Url, mLogiKey)
         'TextBox1.Text = result.Item1
         'DataGridView1.AddObjDatas(result.Item2.Customers)
 
-        Dim service = New CustomerService("https://ivanleathercraft.myshopify.com", "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")
+        Dim service = New CustomerService(_retailEnv("shop_api"), _retailEnv("access_token"))
         Dim result As ListResult(Of ShopifySharp.Customer) = Await service.ListAsync()
         DataGridView1.AddObjDatas(result.Items.ToList())
 
-        'Using WebRequest
-        'Dim myWebRequest As WebRequest = WebRequest.Create(Url)
-        'myWebRequest.ContentType = "application/JSON"
-        'myWebRequest.Method = "GET"
-        'myWebRequest.Headers.Add("Authorization", LoginKey)
-        'Dim myWebResponse As WebResponse = myWebRequest.GetResponse()
-        'Dim StreamRead As StreamReader = New StreamReader(myWebResponse.GetResponseStream())
-        'Dim ProductsJSON As String = StreamRead.ReadToEnd()
-        'Me.TextBox1.Text = ProductsJSON
-        'Dim obj = JsonConvert.DeserializeObject(Of CustomerList)(ProductsJSON)
-        'DataGridView1.AddObjDatas(obj.Customers)
-
-        MsgBox("OK !")
     End Sub
 
     Private Shared Async Function GetRequestWithHttpClient(Of T)(url As String, mLogiKey As Byte()) As Task(Of Tuple(Of String, T))
